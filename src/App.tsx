18dc91cbc151d3f37d3c296c6156e2185a626665
903958734@qq.com <903958734@qq.com>
2021-01-19 00:05:32 +0800

    导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -7,42 +7,69 @@ import {
     Redirect,
     useLocation
 } from "react-router-dom";
+import styled from "styled-components";
+import './index.scss'
+
+const Wrapper = styled.div`
+    min-height: 100vh;
+    overflow: auto;
+    display: flex;
+    flex-direction: column;
+`;
+const Main =styled.div`
+    flex-grow: 1;
+`;
+
+const Nav =styled.nav`
+    >ul{
+    display: flex;
+    >li{
+        width: 33.3333333%;
+        text-align: center;
+        padding: 16px;
+    }
+}
+
+`
 
 function App() {
-    return(<Router>
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/tags">标签页</Link>
-                    </li>
-                    <li>
-                        <Link to="/money">记账页</Link>
-                    </li>
-                    <li>
-                        <Link to="/statistics">统计页</Link>
-                    </li>
-                </ul>
-            </nav>
+    return(
+        <Router>
+            <Wrapper>
+                <Main>
+                <Switch>
+                    <Route path="/tags">
+                        <Tags/>
+                    </Route>
+                    <Route path="/money">
+                        <Money/>
+                    </Route>
+                    <Route path="/statistics">
+                        <Statistics/>
+                    </Route>
+                    <Redirect exact from="/" to="/money"/>
+                    <Route path="*">
+                        <NoMatch />
+                    </Route>
+                </Switch>
+                </Main>
+                <Nav>
+                    <ul>
+                        <li>
+                            <Link to="/tags">标签页</Link>
+                        </li>
+                        <li>
+                            <Link to="/money">记账页</Link>
+                        </li>
+                        <li>
+                            <Link to="/statistics">统计页</Link>
+                        </li>
+                    </ul>
+                </Nav>
 
-
-            <Switch>
-                <Route path="/tags">
-                    <Tags/>
-                </Route>
-                <Route path="/money">
-                    <Money/>
-                </Route>
-                <Route path="/statistics">
-                    <Statistics/>
-                </Route>
-                <Redirect exact from="/" to="/money"/>
-                <Route path="*">
-                    <NoMatch />
-                </Route>
-            </Switch>
-        </div>
-    </Router>)
+            </Wrapper>
+        </Router>
+    )
 }
 
 function Statistics() {
