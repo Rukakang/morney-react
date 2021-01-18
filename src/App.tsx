502f01d49e211eb02cf97c4a2d971a7c685e7015
903958734@qq.com <903958734@qq.com>
2021-01-18 23:05:22 +0800

    安装react-router-dom,并按官网示例完成路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,72 @@
-import React from 'react';
-import styled from "styled-components";
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+    useLocation
+} from "react-router-dom";
 
-
-const Button =styled.button`
-color:red;
-border: none;
-background: green;
-&:hover{
-background: black;
-}
-`
 function App() {
-  return (
-    <div>
-      <Button>你好</Button>
-    </div>
-  );
+    return(<Router>
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/tags">标签页</Link>
+                    </li>
+                    <li>
+                        <Link to="/money">记账页</Link>
+                    </li>
+                    <li>
+                        <Link to="/statistics">统计页</Link>
+                    </li>
+                </ul>
+            </nav>
+
+
+            <Switch>
+                <Route path="/tags">
+                    <Tags/>
+                </Route>
+                <Route path="/money">
+                    <Money/>
+                </Route>
+                <Route path="/statistics">
+                    <Statistics/>
+                </Route>
+                <Redirect exact from="/" to="/money"/>
+                <Route path="*">
+                    <NoMatch />
+                </Route>
+            </Switch>
+        </div>
+    </Router>)
 }
+
+function Statistics() {
+    return <h2>统计页</h2>;
+}
+
+function Tags() {
+    return <h2>标签页</h2>;
+}
+
+function Money() {
+    return <h2>记账页</h2>;
+}
+function NoMatch() {
+    let location = useLocation();
+
+    return (
+        <div>
+            <h3>
+                404 NOT FOUND <code>{location.pathname}</code>
+            </h3>
+        </div>
+    );
+}
+
 
 export default App;
