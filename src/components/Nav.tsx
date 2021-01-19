f0a8b536bcc64203f03d0d479a97546c998accfc
903958734@qq.com <903958734@qq.com>
2021-01-19 22:35:05 +0800

    当前路由图标自动高亮

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import React from "react";
 import Icon from "./Icon";
 
@@ -22,6 +22,9 @@ const NavWrapper =styled.nav`
               width: 24px;
               height: 24px;
             }
+            &.selected{
+              color: #4119ff;
+            }
         }
         
         
@@ -33,24 +36,22 @@ const Nav = () =>{
         <NavWrapper>
             <ul>
                 <li>
-                    <Link to="/tags">
+                    <NavLink to="/tags" activeClassName="selected">
                         <Icon name="label"/>
                         标签页
-                    </Link>
+                    </NavLink>
                 </li>
                 <li>
-
-                    <Link to="/money">
+                    <NavLink to="/money" activeClassName="selected">
                         <Icon name="money"/>
                         记账页
-                    </Link>
+                    </NavLink>
                 </li>
                 <li>
-
-                    <Link to="/statistics">
+                    <NavLink to="/statistics" activeClassName="selected">
                         <Icon name="chart"/>
                         统计页
-                    </Link>
+                    </NavLink>
                 </li>
             </ul>
         </NavWrapper>
