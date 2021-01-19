57a2314b8aac34ee2e72709669518fe98d26643d
903958734@qq.com <903958734@qq.com>
2021-01-19 15:31:06 +0800

    重构代码，页面跳转时需要导航栏才加导航栏，404不需要导航栏

--- src/App.tsx
+++ src/App.tsx
@@ -23,50 +23,66 @@ const Main =styled.div`
 function App() {
     return(
         <Router>
-            <Wrapper>
-                <Main>
-                <Switch>
-                    <Route path="/tags">
-                        <Tags/>
-                    </Route>
-                    <Route path="/money">
-                        <Money/>
-                    </Route>
-                    <Route path="/statistics">
-                        <Statistics/>
-                    </Route>
-                    <Redirect exact from="/" to="/money"/>
-                    <Route path="*">
-                        <NoMatch />
-                    </Route>
-                </Switch>
-                </Main>
-                <Nav/>
-            </Wrapper>
+            <Switch>
+                <Route path="/tags">
+                    <Tags/>
+                </Route>
+                <Route path="/money">
+                    <Money/>
+                </Route>
+                <Route path="/statistics">
+                    <Statistics/>
+                </Route>
+                <Redirect exact from="/" to="/money"/>
+                <Route path="*">
+                    <NoMatch />
+                </Route>
+            </Switch>
         </Router>
     )
 }
 
 function Statistics() {
-    return <h2>统计页</h2>;
+    return (
+        <Wrapper>
+            <Main>
+                <h2>统计页</h2>
+            </Main>
+            <Nav/>
+        </Wrapper>
+    );
 }
 
 function Tags() {
-    return <h2>标签页</h2>;
+    return (
+        <Wrapper>
+            <Main>
+                <h2>标签页</h2>
+            </Main>
+            <Nav/>
+        </Wrapper>
+    );
 }
 
 function Money() {
-    return <h2>记账页</h2>;
+    return (
+        <Wrapper>
+            <Main>
+                <h2>记账页</h2>
+            </Main>
+            <Nav/>
+        </Wrapper>
+    );
 }
 function NoMatch() {
     let location = useLocation();
 
     return (
-        <div>
-            <h3>
-                404 NOT FOUND <code>{location.pathname}</code>
-            </h3>
-        </div>
+        <Wrapper>
+            <Main>
+                <h3>404 NOT FOUND <code>{location.pathname}</code></h3>
+            </Main>
+        </Wrapper>
     );
 }
 
