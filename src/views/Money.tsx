a596843e5a48c2b1d0392b9cf1f11506824e7c6c
903958734@qq.com <903958734@qq.com>
2021-01-20 18:40:13 +0800

    完成Money.tsx中NumberPad的css样式

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -2,6 +2,29 @@ import Layout from "../components/Layout";
 import React from "react";
 import styled from "styled-components";
 
+const TagsSection = styled.section`
+    background: #FFFFFF;  
+    padding: 12px 16px;
+    >ol{
+    margin: 0 -12px;
+      >li{
+        background: #f5f5f5;
+        border-radius: 18px;
+        display: inline-block;
+        padding: 3px 18px;
+        margin: 8px 12px;
+        font-size: 14px;
+      }
+    }
+    >button{
+      background: none;
+      border: none;
+      border-bottom: 1px solid #333;
+      color: #666;
+      padding: 2px 4px;
+      margin-top: 8px;
+    }
+`;
 const NotesSection =styled.section`
   background: #f5f5f5;
   padding: 0 16px;
@@ -47,30 +70,53 @@ const CategorySection = styled.section`
   }
 `;
 const NumberPadSection =  styled.section`
-
-`;
-const TagsSection = styled.section`
-    background: #FFFFFF;  
-    padding: 12px 16px;
-    >ol{
-    margin: 0 -12px;
-      >li{
-        background: #f5f5f5;
-        border-radius: 18px;
-        display: inline-block;
-        padding: 3px 18px;
-        margin: 8px 12px;
-        font-size: 14px;
+  display: flex;
+  flex-direction: column;
+  >.output{
+    background: white;
+    text-align: right;
+    font-size: 36px;
+    font-family: Consolas,monospace;
+    line-height: 72px;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
+                inset 0 5px 5px -5px rgba(0,0,0,0.25);
+  }
+  >.pad{
+    >button{
+      float: left;
+      width: 25%;
+      height: 64px;
+      border: none;
+      font-size: 18px;
+      &.ok{
+      float: right;
+        height: 128px;
+      }
+      &.zero{
+       width: 50%;   
+      }
+  
+      &:nth-child(1){
+        background: #f2f2f2;
+      }
+      &:nth-child(2),&:nth-child(5){
+        background: #E0E0E0;
+      }
+      &:nth-child(3),&:nth-child(6),&:nth-child(9){
+        background: #D3D3D3;
+      }
+      &:nth-child(4),&:nth-child(7),&:nth-child(10),&:nth-child(13){
+        background: #C1C1C1;
+      }
+      &:nth-child(8),&:nth-child(11),&:nth-child(14){
+        background: #B8B8B8;
+      }
+      &:nth-child(12){
+        background:  #9A9A9A;
       }
     }
-    >button{
-      background: none;
-      border: none;
-      border-bottom: 1px solid #333;
-      color: #666;
-      padding: 2px 4px;
-      margin-top: 8px;
-    }
+  }
 `;
 
 function Money() {
@@ -98,10 +144,10 @@ function Money() {
                 </ul>
             </CategorySection>
             <NumberPadSection>
-                <div>
+                <div className="output">
                     100
                 </div>
-                <div>
+                <div className="pad clearfix">
                     <button>1</button>
                     <button>2</button>
                     <button>3</button>
@@ -113,9 +159,9 @@ function Money() {
                     <button>7</button>
                     <button>8</button>
                     <button>9</button>
-                    <button>OK</button>
-                    <button>0</button>
+                    <button className="ok">OK</button>
                     <button>.</button>
+                    <button className="zero">0</button>
                 </div>
             </NumberPadSection>
         </Layout>
