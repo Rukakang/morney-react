e6201fccfb58461fbb11715bcf11c8eb137064b7
903958734@qq.com <903958734@qq.com>
2021-01-25 17:11:29 +0800

    重构Money.vue的NumberpadSection
    Signed-off-by: 903958734@qq.com <903958734@qq.com>

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,4 +1,3 @@
-import styled from "styled-components";
 import React, {useState} from "react";
 import Wrapper from "./NumberPadSection/Wrapper";
 import generateOutput from "./NumberPadSection/generateOutput";
